c862c6a58d6621ee6f8886a11eea7da3331f5e5d
aniketraut16 <rautnaniket@gmail.com>
2026-04-14 13:36:10 +0530

    feat: Add TrackingUpdatesModal component and integrate tracking updates functionality in OrderPage, enhancing order tracking experience for users

--- src/components/Perfume/HeroSlideshow.tsx
+++ src/components/Perfume/HeroSlideshow.tsx
@@ -4,10 +4,9 @@ import { useState, useEffect } from "react";
 
 export default function HeroSlideshow() {
   const heroImages = [
-    "/perfume/blue.png",
-    "/perfume/black.png",
-    "/perfume/white.png",
-    "/perfume/off-white.png",
+    "/perfume/orange.png",
+    "/perfume/pink.png",
+    "/perfume/cyan.png",
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,144 +27,114 @@ export default function HeroSlideshow() {
 
   const currentImage = heroImages[currentIndex];
   const previousImage = heroImages[prevIndex];
- 
+
   return (
     <div className="relative w-full h-screen overflow-hidden bg-black">
       {/* Previous Image Layer - slides out */}
       <div className="absolute inset-0 flex items-center justify-center">
-    <div 
+        <div
           key={`${prevIndex}-prev-0`}
           className="h-full flex-1 hidden lg:block animate-[slideToBottom_1s_ease-out_forwards]"
-      style={{
+          style={{
             backgroundImage: `url(${previousImage})`,
-        backgroundSize: '500% 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '0% 0'
-      }}
-    >
-        </div>
-        <div 
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "0% 0",
+          }}
+        ></div>
+        <div
           key={`${prevIndex}-prev-1`}
           className="h-full flex-1 animate-[slideToTop_1s_ease-out_forwards]"
           style={{
             backgroundImage: `url(${previousImage})`,
-            backgroundSize: '500% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '25% 0'
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "33.333% 0",
           }}
-        >
-        </div>
-        <div 
+        ></div>
+        <div
           key={`${prevIndex}-prev-2`}
           className="h-full flex-1 animate-[slideToBottom_1s_ease-out_forwards]"
           style={{
             backgroundImage: `url(${previousImage})`,
-            backgroundSize: '500% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '50% 0'
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "66.666% 0",
           }}
-        >
-        </div>
-        <div 
+        ></div>
+        <div
           key={`${prevIndex}-prev-3`}
           className="h-full flex-1 hidden md:block animate-[slideToTop_1s_ease-out_forwards]"
           style={{
             backgroundImage: `url(${previousImage})`,
-            backgroundSize: '500% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '75% 0'
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "100% 0",
           }}
-        >
-        </div>
-        <div 
-          key={`${prevIndex}-prev-4`}
-          className="h-full flex-1 hidden lg:block animate-[slideToBottom_1s_ease-out_forwards]"
-          style={{
-            backgroundImage: `url(${previousImage})`,
-            backgroundSize: '500% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '100% 0'
-          }}
-        >
-        </div>
+        ></div>
       </div>
 
       {/* Current Image Layer - slides in */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <div 
+        <div
           key={`${currentIndex}-0`}
           className="h-full flex-1 hidden lg:block animate-[slideFromTop_1s_ease-out_forwards]"
           style={{
             backgroundImage: `url(${currentImage})`,
-            backgroundSize: '500% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '0% 0'
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "0% 0",
           }}
-        >
-    </div>
-    <div 
+        ></div>
+        <div
           key={`${currentIndex}-1`}
-      className="h-full flex-1 animate-[slideFromBottom_1s_ease-out_forwards]" 
-      style={{
+          className="h-full flex-1 animate-[slideFromBottom_1s_ease-out_forwards]"
+          style={{
             backgroundImage: `url(${currentImage})`,
-        backgroundSize: '500% 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '25% 0'
-      }}
-    >
-    </div>
-    <div 
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "33.333% 0",
+          }}
+        ></div>
+        <div
           key={`${currentIndex}-2`}
-      className="h-full flex-1 animate-[slideFromTop_1s_ease-out_forwards]" 
-      style={{
+          className="h-full flex-1 animate-[slideFromTop_1s_ease-out_forwards]"
+          style={{
             backgroundImage: `url(${currentImage})`,
-        backgroundSize: '500% 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '50% 0'
-      }}
-    >
-    </div>
-    <div 
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "66.666% 0",
+          }}
+        ></div>
+        <div
           key={`${currentIndex}-3`}
           className="h-full flex-1 hidden md:block animate-[slideFromBottom_1s_ease-out_forwards]"
-      style={{
+          style={{
             backgroundImage: `url(${currentImage})`,
-        backgroundSize: '500% 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '75% 0'
-      }}
-    >
-    </div>
-    <div 
-          key={`${currentIndex}-4`}
-          className="h-full flex-1 hidden lg:block animate-[slideFromTop_1s_ease-out_forwards]"
-      style={{
-            backgroundImage: `url(${currentImage})`,
-        backgroundSize: '500% 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '100% 0'
-      }}
-    >
-        </div>
+            backgroundSize: "400% 100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "100% 0",
+          }}
+        ></div>
       </div>
-    
-    <style jsx>{`
-      @keyframes slideFromTop {
-        from {
-          transform: translateY(-100%);
+
+      <style jsx>{`
+        @keyframes slideFromTop {
+          from {
+            transform: translateY(-100%);
+          }
+          to {
+            transform: translateY(0);
+          }
         }
-        to {
-          transform: translateY(0);
+        @keyframes slideFromBottom {
+          from {
+            transform: translateY(100%);
+          }
+          to {
+            transform: translateY(0);
+          }
         }
-      }
-      @keyframes slideFromBottom {
-        from {
-          transform: translateY(100%);
-        }
-        to {
-          transform: translateY(0);
-        }
-      }
         @keyframes slideToTop {
           from {
             transform: translateY(0);
@@ -182,7 +151,7 @@ export default function HeroSlideshow() {
             transform: translateY(100%);
           }
         }
-    `}</style>
-   </div>
+      `}</style>
+    </div>
   );
 }
